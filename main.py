4300c267d087e8f87363d5dd56c93ce27b7dfd30
Alex <25344028+MrZomka@users.noreply.github.com>
2023-04-21 09:59:37 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import time, shutil, os
+
+def measure(size):
+    with open(f"./.dsdst/{size}_temp", "wb") as out:
+        out.truncate(size * 1024 * 1024)
+    startWrite=time.time()
+    shutil.copyfile(f"./.dsdst/{size}_temp", f"./.dsdst/{size}_temp_copied")
+    endWrite=time.time()
+
+    print(f"{size}MB ~ WRITE -> {round(size/(endWrite-startWrite), 2)} megabytes per second")
+
+    chunk_size = 4096
+
+    startRead=time.time()
+    with open(f"./.dsdst/{size}_temp", "rb") as in_file:
+        while True:
+            chunk = in_file.read(chunk_size)
+            
+            if chunk == b"":
+                break
+    endRead=time.time()
+    print(f"{size}MB ~ READ -> {round(size/(endRead-startRead), 2)} megabytes per second")
+    os.remove(f"./.dsdst/{size}_temp")
+    os.remove(f"./.dsdst/{size}_temp_copied")
+
+path = ".dsdst"
+if not os.path.exists(path):
+   os.makedirs(path)
+   print("The temporary directory was created.")
+
+measure(256)
+measure(512)
+measure(1024)
+measure(2048)
+measure(4096)
